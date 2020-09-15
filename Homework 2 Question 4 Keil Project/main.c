d0b31efa44cd7d788a0172513e8f62f7f988df98
harry k <harrisonftl@gmail.com>
2020-09-14 21:18:19 -0400

    question 5 done

--- Homework 2 Question 4 Keil Project/main.c
+++ Homework 2 Question 4 Keil Project/main.c
@@ -10,15 +10,79 @@
 
 
 
+/* Global variables here – avoid declaring too many globals unnecessarily as it is not considered a good
+programming practice. */
+int creditRatingAlice = 750;
+// 0 represents “defaulted” and 1 represents “paid” for the payment history
+int monthlyPaymentHistoryAlice[24] = {1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1};
 
+//global credit status flag
+volatile int creditStatus;
 // Called by startup assembly code, start of C code
-int main(void){    
 	
+volatile int month = 23;
+/* Function called when Alice makes a monthly payment or defaults on the monthly payment.
+* Returns nothing, updates global variable creditRatingAlice. */
+void updateCreditRatingAlice (int opcode) 
+{
 	
-	
-  while(1){
+// each monthly payment increments rating by 10 points all the way up to 800
+// every default decrements rating by 10 points down to 700.
+	if(opcode == 1 && creditRatingAlice != 800)
+	{
+		creditRatingAlice += 10;
+	}
+	else if(opcode == 0 && creditRatingAlice != 700)
+	{
+		creditRatingAlice -= 10;
+	}
+}
+/* Function that raises an alarm when Alice's rating is 700 for 2 consecutive months,
+* or rewards her if she has maintained a rating of 800 for 2 consecutive months.
+* Reads montlyPaymentHistoryAlice for input.
+* Returns 0 if alarm, 1 if reward. */
+int rewardsOrAlarm () 
+{
+ // Read next value in monthlyPaymentHistoryAlice. Note that this function needs to keep track of the
+ // last array element accessed so that when it reaches the last it can rewind to the first in a round
+ // robin fashion to read a value from the monthlyPaymentHistoryAlice array forever.
+// Pass the read value in monthlyPaymentHistoryAlice to function updateCreditRatingAlice(int x).
+// Return reward/alarm value.
+
+	while(1)
+	{
+		month = (month + 1) % 24;
+		if(monthlyPaymentHistoryAlice[month] == 1)
+		{
+			if(creditRatingAlice == 800)
+			{
+				return 1;
+			}
+			updateCreditRatingAlice(1);
+		}
+		else
+		{
+			if(creditRatingAlice == 700)
+			{
+				return 0;
+			}
+			updateCreditRatingAlice(0);
+		}
 		
+		
+	}
+	return 1;
+}
+int main(void)
+{    
+	
+	// main(), while looping endlessly, sets a global flag creditStatus to the value returned by the function
+// rewardsOrAlarm(). 
+  while(1)
+	{
+		creditStatus = rewardsOrAlarm();
   }
 }
+
 
 
--- Homework 2 Question 5 Keil Project/main.c
+++ Homework 2 Question 5 Keil Project/main.c
@@ -12,11 +12,47 @@
 
 
 // Called by startup assembly code, start of C code
-int main(void){    
+
+#define TRUE 1
+#define FALSE 0
+#define N 25 // array size
+
+unsigned long sum(unsigned long n)
+{
+	unsigned long partial = 0;
+	while(n > 0)
+	{
+		partial += n;
+		n--;
+	}
 	
+	return partial;
+}
+unsigned long fun(unsigned long n)
+{
+	return (n *(n+1))/2;
+}
+int main(void)
+{    
 	
+	volatile unsigned long num;
+	volatile unsigned long sumBuf[N], funBuf[N]; // arrays to store computed values
+	volatile int correctFlag = TRUE;
 	
-  while(1){
+	for(num = 0; num < N; num++)
+	{
+		sumBuf[num] = sum(num);
+		funBuf[num] = fun(num);
+		if(sumBuf[num] != funBuf[num])
+		{
+			correctFlag = FALSE;
+		}				
+	}
+	
+	return correctFlag;
+	
+  while(1)
+	{
 		
   }
 }
